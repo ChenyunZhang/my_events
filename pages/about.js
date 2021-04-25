78aed6a078248b48564ebd40c9df6b9f7dabbd97
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-25 16:24:42 -0400

    router

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function AboutPage() {
     return (
         <div>
             <h1>About</h1>
+            <Link href="/">Home</Link>
             <p>This is an app to manage your events</p>
             <p>🥰</p>
         </div>
