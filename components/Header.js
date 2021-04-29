fe94d7ce5c360f859fa58b55ebaf42f0ba89242b
Chenyun Zhang <morningchenyun@gmail.com>
2021-04-28 21:25:10 -0400

    update

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,14 @@ import styles from "../styles/Header.module.css"
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.logo}></div>
-            <Link href="/">
-                <a>Event.com</a>
-            </Link>
-
+            <div className={styles.logo}>
+                <Link href="/">
+                    <a>Events.com</a>
+                </Link>
+            </div>
             <nav>
                 <ul>
-                    <li><Link href="/event"><a>Events</a></Link></li>
+                    <li><Link href="/events"><a>Events</a></Link></li>
                     <li>LOGIN</li>
                 </ul>
             </nav>
--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ export default function Layout({ title, keywords, description, children }) {
                 <meta name="keywords" content={keywords} />
             </Head>
             <Header />
-            <div classNamE="styles.container">
+            <div className="styles.container">
                 {children}
             </div>
             <Footer/>
